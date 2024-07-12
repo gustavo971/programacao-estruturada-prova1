3eaea61d009bc7da68d08c0885a23ff5c362941c
Gustavo <150375641+gustavo971@users.noreply.github.com>
2024-07-12 19:43:10 +0000

    rs

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 def q1():
-    pass
+    numero = int(input())
+
+    contador = 0
+    for i in range(1,numero+1):
+        if numero % i == 0 and i % 3 == 0:
+            contador += 1
+
+    if contador == o:
+        print("O número não possui divisores multiplos de 3,contador")
+
+    else:
+        print("Quantidade de divisores divisiveis po 1")
 
 def q2():
-    pass
+    menor = int(input(""))
+    maior = int(input(""))
+    if menor > maior:
+        menor, maior = maior, menor
+
+
+    soma = 0
+
+    for i in range(menor, maior+1):
+        if i > 0:
+            soma += i
+
+    print(soma)
 
 def q3():
     pass
